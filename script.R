3988e07dc8865eb9ee7bf0572b2a8c0d3579592f
Elsemieke van Gent <elsemiekevangent@macbookair-1.home>
2026-09-02 17:01:56 +0200

    I finalised the R script for both loading the data and saving the data into data/raw

--- script.R
+++ script.R
@@ -1,2 +1,8 @@
+# Load the data set and save it in the data:
 library("tidyverse")
 data <- read_csv("https://raw.githubusercontent.com/hannesdatta/course-dprep/refs/heads/main/material/project/video_view.csv")
+write_csv(data, "data/raw/video_view.csv")
+
+#Load the data set from the data section:
+library("tidyverse")
+data <- read_csv("data/raw/video_view.csv")
