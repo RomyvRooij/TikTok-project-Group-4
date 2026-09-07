28576d27f349d0940aae7c502e284bd7295bbd77
Romy van Rooij <romy@mac-1.home>
2026-09-07 20:44:44 +0200

    Finalize the summary

--- src/Downloaddata.R
+++ src/Downloaddata.R
@@ -2,6 +2,9 @@ library("tidyverse")
 url <- "https://raw.githubusercontent.com/hannesdatta/course-dprep/refs/heads/main/material/project/video_view.csv"
 destination <- "data/raw/video_view.csv"
 
+#to create the folder when it isn't there yet
+dir.create("data/raw", recursive = TRUE, showWarnings = FALSE)
+
 # Check to see if the file already exists and otherwise download it:
 if (!file.exists(destination)) {
   data <- read_csv(url)
