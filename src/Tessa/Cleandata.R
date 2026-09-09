74a1197ca47c46ef36a794d6ca6ee287302a677e
TessaBreusers <t.e.e.breusers@tilburguniversity.edu>
2026-09-09 14:05:28 +0200

    cleaned the data by making column names lowercase and then stored the cleaned data in data/processed

--- src/Tessa/Cleandata.R
+++ src/Tessa/Cleandata.R
@@ -3,3 +3,6 @@ users <- read.csv("data/raw/users.csv")
 
 # Make column names lowercase
 names(users) <- tolower(names(users))
+
+# Save cleaned data
+write.csv(users, "data/processed/users_clean.csv", row.names = FALSE)
