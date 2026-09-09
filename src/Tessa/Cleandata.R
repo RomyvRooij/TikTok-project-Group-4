e534c3e54ece68a8d5c1b780cb87987c8f0d5a32
TessaBreusers <t.e.e.breusers@tilburguniversity.edu>
2026-09-09 14:30:25 +0200

    changes in cleaning data

--- src/Tessa/Cleandata.R
+++ src/Tessa/Cleandata.R
@@ -4,5 +4,8 @@ users <- read.csv("data/raw/users.csv")
 # Make column names lowercase
 names(users) <- tolower(names(users))
 
+#Delete missing values
+users <- na.omit(users)
+
 # Save cleaned data
 write.csv(users, "data/processed/users_clean.csv", row.names = FALSE)
