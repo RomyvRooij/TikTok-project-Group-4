80d345cf38a9b6d81ccb840a40437b56e30ec2ea
Elsemieke van Gent <elsemiekevangent@MacBook-Air.local>
2026-09-18 13:12:06 +0200

    I changed the location of the output, now it has its separate folder in output/user_analysis

--- src/tiktok-user-analysis/Visualize.R
+++ src/tiktok-user-analysis/Visualize.R
@@ -40,10 +40,10 @@ plot_preferences <- ggplot(avg_preferences, aes(x = category, y = mean_preferenc
   theme_minimal()
 
 # Create output folder if it does not exist
-dir.create("output", showWarnings = FALSE)
+dir.create("output/user_analysis", showWarnings = FALSE)
 
 # Save plot
-ggsave("output/average_preferences.png", plot = plot_preferences,
+ggsave("output/user_analysis/average_preferences.png", plot = plot_preferences,
        width = 8, height = 5, dpi = 300)
 
 plot_preferences
